4bfaf856216c5b130e9a325401cf38090ed6bb96
Sri Nuraeni <93548932+srinurrr@users.noreply.github.com>
2022-01-10 06:19:27 +0700

    Add files via upload

---
+++ app/Http/Controllers/LatihanController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LatihanController extends Controller
+{
+    public function index() {
+        return "oke, ini dari controller";
+    }
+
+    public function blog($id) {
+        return "Ini blog dengan id ".$id;
+    }
+
+    public function komentar($idblog, $idkomentar) {
+        echo 'Id blognya : '.$idblog;
+        echo '<br />';
+        echo 'Id komentarnya : '.$idkomentar;
+    }
+
+    public function beranda() {
+        $data = array('nama' => 'Sri Nuraeni');
+        return view('beranda', $data);
+    }
+}
